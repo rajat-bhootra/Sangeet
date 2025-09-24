953fec83f209bc753309008030fea1779826c2ee
rajat-bhootra <rajatbhootra7@gmail.com>
2025-09-24 10:10:21 +0530

    playbar and playlist

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 console.log('lets do JS');
+
+async function getSongs() {
+    let a = await fetch("http://10.32.6.12:3000/songs/")
+    let response = await a.text();
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")){
+            songs.push(element.href.split("/songs/")[1])
+        }
+        
+    }
+    return songs
+}
+
+async function main() {
+    // get the list of all songs
+    let songs = await getSongs()
+    console.log(songs);
+
+    let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `
+                            <li> 
+                            <img class="invert" src="music.svg" alt="music">
+                            <div class="info">
+                                <div>${song.replaceAll("%20"," ")}</div>
+                                <div>Song Artist</div>
+                            </div>
+                            <img class="invert" src="play.svg" alt="play">
+                            </li>`;
+    }    
+
+    //play the first song
+    var audio = new Audio(songs[0]);
+    audio.play();
+}
+
+main()
